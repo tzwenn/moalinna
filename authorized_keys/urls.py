4c3b357a55552881cd47ed4e1f492e590456c88a
Sven Koehler <sven.koehler@hpi.de>
2020-12-18 14:29:14 +0100

    Basic add and delete list

--- authorized_keys/urls.py
+++ authorized_keys/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = 'authorized_keys'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('add/', views.add, name='add'),
+    path('delete/<int:key_id>/', views.delete, name='delete'),
 ]
--- authorized_keys/views.py
+++ authorized_keys/views.py
@@ -1,5 +1,16 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
+from .models import PubSSHKey
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+	pubsshkey_list = PubSSHKey.objects.all()
+	context = {'pubsshkey_list': pubsshkey_list}
+	return render(request, 'authorized_keys/index.html', context)
+
+
+def add(request):
+	return HttpResponse("Not added")
+
+def delete(request, key_id):
+	return HttpResponse("Key %d not deleted" % key_id)
